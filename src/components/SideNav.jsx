c019e756382b5a77ef46d1051f3dc53f66e9dff6
Chinenye <birdievsq@gmail.com>
2025-06-15 22:26:49 +0100

    Restoring data.js file
    :wq

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
-import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import {
   LayoutDashboard,
   Users,
